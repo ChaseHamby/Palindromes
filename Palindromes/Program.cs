94962ab69fead0296549f03f00d92bd2825ddb3a
unknown <c.chase.hamby@gmail.com>
2019-03-09 11:38:27 -0600

    bool working but not detecting pals with whitespaces

---
+++ Palindromes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Palindromes
+{
+    class Program
+    {
+        public static bool IsPalindrome(string myString)
+        {
+            return myString.SequenceEqual(myString.Reverse());
+        }
+        static void Main(string[] args)
+        {
+            string[] array =
+            {
+            "Stats",
+            "deified",
+            "deleveled",
+            "No X in Nixon",
+            "dewed",
+            "Hannah",
+            "kayak",
+            "Was it a cat I saw",
+            "madam",
+            "rotor",
+            "sagas",
+            "Dot",
+            "Net",
+            "Is",
+            "Not",
+            "A",
+            "Palindrome",
+            ""
+            };
+            foreach (string value in array)
+            {
+                Console.WriteLine("{0} = {1}", value, IsPalindrome(value));
+                Console.ReadKey();
+            }
+        }
+    }
+}
